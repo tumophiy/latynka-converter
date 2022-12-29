4444e6e7b7edc1ac064e9317ea394c34f9be10f6
tim <tima.muravyov@gmail.com>
2022-12-29 22:26:31 +0200

    feat: add basic translation logic

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@
 
 source 'https://rubygems.org'
 
+gem 'dotenv', '~> 2.8', '>= 2.8.1'
 gem 'telegram-bot-ruby', '~> 0.23.0'
--- main.rb
+++ main.rb
@@ -1,40 +1,90 @@
+# frozen_string_literal: true
+
+require 'dotenv/load'
 require 'telegram/bot'
 
-TOKEN = '5852461356:AAFzUSlr3Rnk8O2ETfcBGQ1NT7z_ZixsYCs'
+# token = ENV['TOKEN']
+token = '5852461356:AAFzUSlr3Rnk8O2ETfcBGQ1NT7z_ZixsYCs'
 
-# Telegram::Bot::Client.run(TOKEN) do |bot|
-#   bot.listen do |message|
-#     case message.text
-#     when '/start', '/start start'
-#       bot.api.send_message(
-#         chat_id: message.chat.id,
-#         text: "3ApaBCTByü, #{message.from.first_name}"
-#       )
-#     else
-#       bot.api.send_message(
-#         chat_id: message.chat.id,
-#         text: 'test'
-#       )
-#     end
-#   end
-# end
+HASH = {
+  'б' => 'b', 'Б' => 'B',
+  'в' => 'v', 'В' => 'V',
+  'г' => 'g', 'Г' => 'G',
+  'ґ' => 'ĝ', 'Ґ' => 'Ĝ',
+  'д' => 'd', 'Д' => 'D',
+  'ж' => 'ž', 'Ж' => 'Ž',
+  'з' => 'z', 'З' => 'Z',
+  'к' => 'k', 'К' => 'K',
+  'л' => 'l', 'Л' => 'L',
+  'м' => 'm', 'М' => 'M',
+  'н' => 'n', 'Н' => 'N',
+  'п' => 'p', 'П' => 'P',
+  'р' => 'r', 'Р' => 'R',
+  'с' => 's', 'С' => 'S',
+  'т' => 't', 'Т' => 'T',
+  'ф' => 'f', 'Ф' => 'F',
+  'х' => 'h', 'Х' => 'H',
+  'ц' => 'c', 'Ц' => 'C',
+  'ч' => 'č', 'Ч' => 'Č',
+  'ш' => 'š', 'Ш' => 'Š',
+  'а' => 'a', 'А' => 'A',
+  'е' => 'e', 'Е' => 'E',
+  'и' => 'y', 'И' => 'Y',
+  'і' => 'i', 'І' => 'I',
+  'о' => 'o', 'О' => 'O',
+  'у' => 'u', 'У' => 'U',
+  'й' => 'j', 'Й' => 'J',
+  'ь' => "'", 'Ь' => "'",
+  'щ' => 'šč', 'Щ' => 'Šč',
+  'я' => 'ja', 'Я' => 'Ja',
+  'ю' => 'ju', 'Ю' => 'Ju',
+  'є' => 'je', 'Є' => 'Je',
+  'ї' => 'ji', 'Ї' => 'Ji',
+  'ьа' => 'ja',
+  'ьу' => 'ju',
+  'ье' => 'je',
+  'ьі' => 'ji',
+  'ьо' => 'jo'
+}
+SUBST = Regexp.union(HASH.keys)
 
-Telegram::Bot::Client.run(TOKEN) do |bot|
+def kyrylytsja_to_latynka(text)
+  array = []
+  array_of_chars = text.chars
+  length_of_text = array_of_chars.length - 1
+  skip_next_element = false
+  array_of_chars.each_with_index do |char, index|
+    if char.match?(/[а-яїєґ]/i) && !skip_next_element
+      if char == 'ь' && index < length_of_text
+        match = array_of_chars[index + 1].match(/[іеуао]/)
+        if match
+          aa = "ь#{match.string}"
+          pp aa
+          array << HASH[aa]
+          skip_next_element = true
+          next
+        end
+      end
+      array << HASH[char]
+      skip_next_element = false
+    elsif !skip_next_element
+      array << char
+    else
+      skip_next_element = false
+    end
+  end
+  array.join('')
+end
+
+Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    else
+      bot.api.send_message(chat_id: message.chat.id, text: kyrylytsja_to_latynka(message.text))
     end
   end
 end
-
-# Telegram::Bot::Client.run(TOKEN) do |bot|
-#   bot.listen do |message|
-#     case message.text
-#     when '/start'
-#       bot.api.send_message(chat_id: message.chat_id, text: 'hello')
-#     end
-#   end
-# end
